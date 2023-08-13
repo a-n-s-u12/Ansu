fda82209d29889630850e13c46a72839c328626b
a-n-s-u12 <142132920+a-n-s-u12@users.noreply.github.com>
2023-08-13 10:18:39 +0530

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,12 @@
+a = input("Enter 1st no. : ")
+b = input("Enter 2nd no. : ")
+a = float(a)
+b = float(b)
+print("The avg of two no. :", (a+b)/2)
+
+# square of a no.
+a = 6
+a *=a
+print("Square of a no. :", a)
+a = pow(2,3)
+print(a)
